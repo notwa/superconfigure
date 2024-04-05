833f497a57f7e0416a9bb135e89a7f86a428b346
Connor Olding <cloningdonor@gmail.com>
2024-04-04 22:20:10 -0700

    kissat: working

--- cli/kissat/BUILD.mk
+++ cli/kissat/BUILD.mk
@@ -6,5 +6,4 @@ $(eval $(call DOWNLOAD_SOURCE,cli/kissat,$(KISSAT_SRC)))
 o/cli/kissat/configured.x86_64: CONFIG_COMMAND = $(BASELOC)/cli/kissat/configure
 o/cli/kissat/configured.aarch64: CONFIG_COMMAND = $(BASELOC)/cli/kissat/configure
 
-o/cli/kissat/built.fat: FATTEN_COMMAND = $(BASELOC)/config/apelink_folder.sh
-o/cli/kissat/built.fat: BINS = bin/kissat
+o/cli/kissat/built.fat: BINS = kissat
